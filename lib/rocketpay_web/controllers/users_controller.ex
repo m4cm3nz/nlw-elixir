b0a7ecd769d6c4454fe7f1dbe2531d9bd69b8c9e
Rafael Mac Menz <rmcmenz@gmail.com>
2021-02-27 20:53:18 -0300

    changes

--- lib/rocketpay_web/controllers/users_controller.ex
+++ lib/rocketpay_web/controllers/users_controller.ex
@@ -1,21 +1,15 @@
 defmodule RocketpayWeb.UsersController do
   use RocketpayWeb, :controller
 
+  alias Rocketpay.User
+
+  action_fallback RocketpayWeb.FallbackController
+
   def create(conn, params) do
-    params
-    |>Rocketpay.create_user()
-    |>handle_response(conn)
-  end
-
-  defp handle_response({:ok, %{result: result}}, conn) do
-    conn
-    |> put_status(:ok)
-    |> json(%{message: "Welcome to Rocketpay API. Here is your number #{result}"})
-  end
-
-  defp handle_response({:error, reason}, conn) do
-    conn
-    |> put_status(:bad_request)
-    |> json(reason)
+    with({:ok, %User{} = user} <- Rocketpay.create_user(params)) do
+      conn
+      |> put_status(:created)
+      |> render("create.json", user: user)
+    end
   end
 end
